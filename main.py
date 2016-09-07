e67d89eefcbca6a40fc7b0f05b7bfbbb2af37b14
neight88 <natemaster888@yahoo.com>
2016-09-07 18:45:42 -0500

    everything up to individual posts

--- main.py
+++ main.py
@@ -1,25 +1,76 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
 import webapp2
+import jinja2
+import os
+from google.appengine.ext import db
 
-class MainHandler(webapp2.RequestHandler):
+# set up jinja
+template_dir = os.path.join(os.path.dirname(__file__), "templates")
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
+                                autoescape = True)
+
+class Handler(webapp2.RequestHandler):
+    def write(self, *args, **kwargs):
+        self.response.write(*args, **kwargs)
+
+    def render_str(self, template, **parameters):
+        t = jinja_env.get_template(template)
+        return t.render(**parameters)
+
+    def render(self, template, **kwargs):
+        self.write(self.render_str(template, **kwargs))
+
+class Blog(db.Model):
+    title = db.StringProperty(required = True)
+    text = db.TextProperty(required = True)
+    created = db.DateTimeProperty(auto_now_add = True)
+
+class MainPage(Handler):
+    def render_front(self, title="", text="", error=""):
+        posts = db.GqlQuery("SELECT * FROM Blog ORDER BY created DESC LIMIT 5")
+        self.render("front.html", title=title, text=text, error=error, posts=posts)
+
     def get(self):
-        self.response.write('Hello world!')
+        self.render_front()
+
+    def post(self):
+        title = self.request.get("form_title")
+        text = self.request.get("form_text")
+        #art = self.request.get("art")
+        if title and blogText:
+            newPost = blog(title=title, text=text)
+            newPost.put()
+            self.redirect("/")
+        else:
+            error = "We need both a title and some text"
+            self.render_front(title, blogText, error)
+
+class postPage(Handler):
+    def render_page(self, title="", text="", error=""):
+        self.render("newPostForm.html", title=title, text=text, error=error)
+
+    def get(self):
+        self.render_page()
+    def post(self):
+        title = self.request.get("form_title")
+        text = self.request.get("form_text")
+
+        if title and text:
+            newPost = Blog(title=title, text=text)
+            newPost.put()
+            self.redirect("/")
+        else:
+            error = "We need both a title and some text"
+            self.render_page(title, text, error)
+
+
+class ViewPostHandler(Handler):
+    def get(self, id):
+        staticPostID = Blog.get_by_id(int(id))
+        #This does not actually display anything when you use the ID
+        self.response.write(str(staticPostID))
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', MainPage),
+    ('/newpost', postPage),
+    (webapp2.Route('/blog/<id:\d+>', ViewPostHandler))
 ], debug=True)
